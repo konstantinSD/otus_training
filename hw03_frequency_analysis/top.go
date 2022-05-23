9c6aad7ae1cb178d960431941a78021b59d33d69
Konstantin Tkachenko <tkachenko_kv@astralnalog.ru>
2022-05-23 10:28:25 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type structMap struct {
+	Value string
+	Key   int
+}
+
+func Top10(s string) []string {
+	uniqueMap := make(map[string]int)
+	words := strings.Fields(s)
+
+	for _, word := range words {
+		uniqueMap[word]++
+	}
+	sortedStruct := make([]structMap, 0, len(uniqueMap))
+	for value, key := range uniqueMap {
+		sortedStruct = append(sortedStruct, structMap{value, key})
+	}
+
+	sort.Slice(sortedStruct, func(i, j int) bool {
+		freqI := sortedStruct[i].Key
+		freqJ := sortedStruct[j].Key
+		if freqI == freqJ {
+			return sortedStruct[i].Value < sortedStruct[j].Value
+		}
+		return freqI > freqJ
+	})
+
+	res := make([]string, 0, 10)
+	for j, uniqueValue := range sortedStruct {
+		if j >= 10 {
+			break
+		}
+		res = append(res, uniqueValue.Value)
+	}
+	return res
 }
