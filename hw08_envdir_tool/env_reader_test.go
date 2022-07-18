d68253af9299913252d7aa610dac40505840716c
Konstantin Tkachenko <tkachenko_kv@astralnalog.ru>
2022-07-18 11:28:53 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,26 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("default test", func(t *testing.T) {
+		envs, _ := ReadDir("testdata/env/")
+		expected := Environment{
+			"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+			"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+			"EMPTY": EnvValue{Value: "", NeedRemove: false},
+			"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+			"UNSET": EnvValue{Value: "", NeedRemove: false},
+		}
+		require.Equal(t, expected, envs)
+	})
+
+	t.Run("no directory", func(t *testing.T) {
+		_, err := ReadDir("test.sh")
+		require.NotEqual(t, nil, err)
+	})
 }
